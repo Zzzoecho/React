9afe7b1596d1c8fa6ba145a9fe57213aad498d2b
Zzzoe <531575302@qq.com>
2017-05-29 13:42:44 +0800

    基本骨架完成

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import Welcome from './Welcome'
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root2')
 );
+
+ReactDOM.render(
+  <Welcome/>,
+  document.getElementById('root')
+)
