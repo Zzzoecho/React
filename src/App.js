700347d2d328bd3d13fdf35d3bc16232ff89bdb8
Zzzoe <531575302@qq.com>
2017-05-30 15:41:07 +0800

    新增todo

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
-      todoList: [
-        {id:1, title:'第一个待办'},
-        {id:2, title:'第二个待办'},
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
 
@@ -21,7 +18,7 @@ class App extends Component {
 
     let todos = this.state.todoList.map((item,index)=>{
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
@@ -31,7 +28,9 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} 
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)}/>
         </div>
         <ol>
           {todos}
@@ -39,6 +38,31 @@ class App extends Component {
       </div>
     )
   }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
