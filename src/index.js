0f9e95c7562efe87487645426a3a7e42a95edbf4
Zzzoe <531575302@qq.com>
2017-06-01 13:28:14 +0800

    新建leancloud

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-// import Welcome from './Welcome'
 
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <Welcome/>,
-//   document.getElementById('root')
-// )
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'E4uMiF4nK3n9vwQAnM14stwV-gzGzoHsz';
+var APP_KEY = 'nTOtTzQupylv3gojmSqOR1Si';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
