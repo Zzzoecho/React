3838b0cdf00e0875211a168d260b822d29f1c023
Zzzoe <531575302@qq.com>
2017-06-06 15:52:20 +0800

    fix

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -3,22 +3,22 @@ import React, { Component } from 'react';
 export default class SignUpForm extends Component {
     render(){
         return(
-            <form className="signUp" onSubmit={this.signUp.bind(this)}> {/*注册*/}
+            <form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/*注册*/}
                 <p>SIGN UP</p>
                 <div className="row">
                     <label><i className="iconfont icon-email"></i></label>
-                    <input type="text" placeholder="Email" value={this.state.formData.email}
-                        onChange={this.changeFormData.bind(this, 'email')} />
+                    <input type="text" placeholder="Email" value={this.props.formData.email}
+                        onChange={this.props.onChange.bind(this, 'email')} />
                 </div>
                 <div className="row">
                     <label><i className="iconfont icon-login"></i></label>
-                    <input type="text" placeholder="User Name" value={this.state.formData.username}
-                        onChange={this.changeFormData.bind(this, 'username')} />
+                    <input type="text" placeholder="User Name" value={this.props.formData.username}
+                        onChange={this.props.onChange.bind(this, 'username')} />
                 </div>
                 <div className="row">
                     <label><i className="iconfont icon-password"></i></label>
-                    <input type="password" placeholder="Password" value={this.state.formData.password}
-                        onChange={this.changeFormData.bind(this, 'password')} />
+                    <input type="password" placeholder="Password" value={this.props.formData.password}
+                        onChange={this.props.onChange.bind(this, 'password')} />
                 </div>
                 <div className="row actions">
                     <button type="submit">SIGN UP</button>
